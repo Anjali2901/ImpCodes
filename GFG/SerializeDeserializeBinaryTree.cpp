518b1c5aab549829163d3ba4eb9ec585fab114d8
Anjali Pandey <LIT2016033@iiita.ac.in>
2019-06-24 19:39:26 +0530

    Update SerializeDeserializeBinaryTree.cpp

--- GFG/SerializeDeserializeBinaryTree.cpp
+++ GFG/SerializeDeserializeBinaryTree.cpp
@@ -1,3 +1,4 @@
+
 void preorder(Node *root,vector<int> &A) {
     if(root==NULL)
      return;
@@ -12,21 +13,24 @@ void inorder(Node *root,vector<int> &A) {
     A.push_back(root->data);
     inorder(root->right,A);
 }
-Node* helper(vector<int>& preorder,int i,int j,vector<int>& inorder,int ii,int jj)
-{
-   if(i >= j || ii >= j)
+Node* help(vector<int>& preorder, int pstart, vector<int>& inorder, int istart, int iend){
+    if(istart>iend)
         return NULL;
-    int mid = preorder[i];
-    auto f = find(inorder.begin() + ii,inorder.begin() + jj,mid);
-    int dis = f - inorder.begin() - ii;
-    Node* root = newNode(mid);
-    root -> left = helper(preorder,i + 1,i + 1 + dis,inorder,ii,ii + dis);
-    root -> right = helper(preorder,i + 1 + dis,j,inorder,ii + dis + 1,jj);
+    Node* root = newNode(preorder[pstart]);
+    int k;
+    for(int i=istart; i<=iend; i++){
+        if(inorder[i] == root->data){
+            k = i;
+            break;
+        }
+    }
+    root->left = help(preorder, pstart+1, inorder, istart, k-1);
+    root->right = help(preorder, pstart+k-istart+1, inorder, k+1, iend);
     return root;
 }
-Node *binaryTree(vector<int> &preorder,vector<int> &inorder)
-{
-    return helper(preorder,0,preorder.size(),inorder,0,inorder.size());
+
+Node* binaryTree(vector<int>& preorder, vector<int>& inorder) {
+    return help(preorder, 0, inorder, 0, inorder.size()-1);
 }
 void serialize(Node *root,vector<int> &A) {
     preorder(root,A);
