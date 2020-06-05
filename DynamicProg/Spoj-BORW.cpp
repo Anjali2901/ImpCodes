c03240052552a2b36900d0d6d9b87c3bfe9df0a2
Anjali Pandey <LIT2016033@iiita.ac.in>
2020-06-05 18:41:54 +0530

    Update Spoj-BORW.cpp

--- DynamicProg/Spoj-BORW.cpp
+++ DynamicProg/Spoj-BORW.cpp
@@ -1,3 +1,5 @@
+Method 1 : 
+	
 #include <bits/stdc++.h>
 #define ll long long
 #define pb push_back
@@ -54,3 +56,74 @@ int main()
 	}
 	return 0;
 }
+__________________________________________________________________________________
+Method 2 : improved recursion
+
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define mp make_pair
+using namespace std;
+#define N 205
+int n,a[N];
+//use this
+int min1(int a,int b,int c)
+{
+    return min(a,min(b,c));
+}
+int BorW(int idx,int prevW,int prevB)
+{
+    // three states for dp as clear by the function call, ans depends on the these three values
+    // the constraints for idx, prevW and prevB - 200 for index and 10^6 for prevB and prevW
+    // so 200*1e6*1e6 dp array can 't be made
+    //if we look closely 
+    //except the first call prevW and prevB are values from array
+    //so instead of storing the values we can just store the indices
+    //now for the initial idx lets chng smthngs start from idx=2  a[0]=-1 and a[1] = 1e7
+    // sizeof dp array now can be 200 * 200 * 200
+    if(idx==n+2) 
+    return 0;
+    int ans;
+    if(a[idx]>a[prevB] && a[idx]<a[prevW])
+    {
+        //all three colors can be assigned
+        ans=min1(BorW(idx+1,idx,prevB),BorW(idx+1,prevW,idx),1+BorW(idx+1,prevW,prevB));
+    }
+    else if(a[idx]>a[prevB])
+    {
+        //it can't be assigned white
+        ans = min(BorW(idx+1,prevW,idx),1+BorW(idx+1,prevW,prevB));
+    }
+    else if(a[idx]<a[prevW])
+    {
+        //it can't be assigned black
+        ans = min(BorW(idx+1,idx,prevB),1+BorW(idx+1,prevW,prevB));
+    }
+    else 
+    {
+        //it can't be assigned white or black
+        ans = 1+BorW(idx+1,prevW,prevB);
+    }
+    return ans;
+}
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	while(1)
+	{
+		cin>>n;
+		if(n==-1)
+		break;
+		a[0]=-1;a[1]=1e7;
+		for(int i=2;i<=n+1;i++)
+		cin>>a[i];
+		cout<<BorW(2,1,0)<<"\n";
+	}
+	return 0;
+}
+
+__________________________________________________________
+Method 3 : DP 
+
