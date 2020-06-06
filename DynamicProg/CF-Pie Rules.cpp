063ce189c598fda98e0bc32574c49fbfcb44ba8c
Anjali Pandey <LIT2016033@iiita.ac.in>
2020-06-07 00:19:58 +0530

    Update CF-Pie Rules.cpp

--- DynamicProg/CF-Pie Rules.cpp
+++ DynamicProg/CF-Pie Rules.cpp
@@ -48,3 +48,58 @@ int main()
 	cout<<sum-bob<<" "<<bob;
 	return 0;
 }
+
+_____________________________________________________________________________________
+
+// Top down dynamic programming
+
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define mp make_pair
+using namespace std;
+#define N 55
+int n,a[N],dp[N][2];
+//dp[i][0] will denotes bob's optimal share if he starts eating at ith index and Bob's chance 
+//and dp[i][1] will denote Bob's optimal share  if he starts eating at ith index 
+//and Alice's chance
+//this is how top down dp differs from bottom up 
+int pies(int idx,int chance)
+{
+	//function to optimize Bob's no of slices since he is the 1st player
+	//chance 0 denotes Bob and chance 1 denotes Alice
+    if(idx==n)
+        return 0; 
+    if(dp[idx][chance]!=-1)
+        return dp[idx][chance];
+    // if bob is the current player means he has the decider token
+    //he can either keep decider token and let alice play
+    int ans;  
+    if(chance == 0)
+    { 
+        ans = max(a[idx]+pies(idx+1,1),pies(idx+1,0));
+    } 
+    else 
+    {
+        ans = min(a[idx]+pies(idx+1,1), pies(idx+1,0));
+    }
+    dp[idx][chance]=ans;
+    return ans;
+}
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	cin>>n;
+	int sum=0;
+	for(int i=0;i<n;i++)
+	{
+		cin>>a[i];
+		sum+=a[i];
+		dp[i][0]=dp[i][1]=-1;
+	}
+	int bob=pies(0,0);
+	cout<<sum-bob<<" "<<bob;
+	return 0;
+}
